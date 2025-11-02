d997646f9fd39a9fa247d0c11a06031daa60edc3
sheena <syeongsn@gmail.com>
2025-11-02 15:46:36 +0800

    chore: fix file type

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthRouter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthRouter>
   </StrictMode>
 );
