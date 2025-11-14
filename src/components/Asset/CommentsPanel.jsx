d8bbfbdeea87fd503af073ea35ff0c811fe295d4
Simon Ong <simonongst@gmail.com>
2025-11-14 23:21:10 +0800

    git commit -m "fix: display owner name"

--- src/components/Asset/CommentsPanel.jsx
+++ src/components/Asset/CommentsPanel.jsx
@@ -38,7 +38,7 @@ function CommentsPanel({ openPanel, onClose, selectedAsset }) {
               <div>
                 <label className="text-md text-black flex gap-2 items-center">
                   <IoPerson />
-                  Owner: {selectedAsset.owner}
+                  Owner: {selectedAsset.owner.name}
                 </label>
               </div>
 
