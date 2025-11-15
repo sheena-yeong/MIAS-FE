5000ae205d79532534c42be3a23bb04ab66b42d2
Simon Ong <simonongst@gmail.com>
2025-11-15 16:50:56 +0800

    feat: add handleSubmit function for password change with input fields of oldPassword, NewPassword and confirmPassword

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,8 +1,95 @@
+import { useState } from "react";
+import { changePassword } from "../services/auth";
+import { useAuth } from "../context/AuthContext";
+
 function Settings() {
+  const { tokens } = useAuth();
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await changePassword(
+        oldPassword,
+        newPassword,
+        confirmPassword,
+        tokens.access
+      );
+      return res;
+    } catch (err) {
+      console.log("Failed to change password", err);
+    }
+  };
+
   return (
     <>
       <h3 className="p-3 text-3xl font-semibold mt-4 ml-2">Settings</h3>
-      <h1 className="p-3 text-xl mt-4 ml-2">Under Construction</h1>
+      <div className="flex items-center justify-left">
+        <div className="max-w-md w-full space-y-8 p-8 bg-white rounded-lg shadow">
+          <div>
+            <h2 className="text-2xl font-bold text-gray-900">
+              Change Password
+            </h2>
+          </div>
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div>
+              <label
+                htmlFor="oldPassword"
+                className="block text-sm font-bold text-gray-700"
+              >
+                Old Password
+              </label>
+              <input
+                type="password"
+                required
+                value={oldPassword}
+                onChange={(e) => setOldPassword(e.target.value)}
+                placeholder="Old Password"
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="newPassword"
+                className="block text-sm font-bold text-gray-700"
+              >
+                New Password
+              </label>
+              <input
+                type="password"
+                required
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+                placeholder="New Password"
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="confirmPassword"
+                className="block text-sm font-bold text-gray-700"
+              >
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                placeholder="Confirm Password"
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-[#ff3a3a] hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-50"
+            >
+              Change Password
+            </button>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
