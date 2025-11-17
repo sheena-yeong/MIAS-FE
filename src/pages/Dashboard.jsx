e6d237948a86351c13bf6336001e5086f48267ff
Simon Ong <simonongst@gmail.com>
2025-11-18 01:00:46 +0800

    chore: change grid to 2 columns layout

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,13 +6,12 @@ import AssetCountByStatus from '../components/Charts/AssetCountByStatus';
 function Dashboard({ assetData }) {
   return (
     <>
-      <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
         {/* <TotalAssets assetData={assetData}/> */}
-        <div className="sm:col-span-2 xl:col-span-1">
-          <AssetCountByStatus assetData={assetData} />
-        </div>
-        <AvailableAssetsByCategory assetData={assetData} />
+        <AssetCountByStatus assetData={assetData} />
+
         <AssetCountByOrigin assetData={assetData} />
+        <AvailableAssetsByCategory assetData={assetData} />
       </div>
     </>
   );
