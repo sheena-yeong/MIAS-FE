9ac41c455aa766b2c4133e82db966e360bb4cdb9
Simon Ong <simonongst@gmail.com>
2025-11-22 00:41:17 +0800

    fix: set setSelectedAssociate to null when delete

--- src/components/Associate/AssociateTable.jsx
+++ src/components/Associate/AssociateTable.jsx
@@ -53,6 +53,7 @@ function AssociateTable({
     try {
       await deleteAssociate(selectedAssociate._id, tokens.access);
       await fetchAssociates();
+      setSelectedAssociate(null);
       setOpenAlert(false);
     } catch (error) {
       console.error('Error deleting associate:', error);
