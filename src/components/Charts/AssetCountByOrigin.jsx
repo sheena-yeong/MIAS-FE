950d888866aa82da0e68c3bb77eafd83fd05820c
Simon Ong <simonongst@gmail.com>
2025-11-21 21:03:06 +0800

    chore: change chart color order

--- src/components/Charts/AssetCountByOrigin.jsx
+++ src/components/Charts/AssetCountByOrigin.jsx
@@ -25,11 +25,11 @@ function AssetCountByOrigin({ assetData }) {
   }));
 
   const COLORS = [
-    '#0088FE',
-    '#00C49F',
-    '#FFBB28',
     '#FF8042',
     '#8884d8',
+    '#FFBB28',
+    '#0088FE',
+    '#00C49F',
     '#82ca9d',
   ];
 
